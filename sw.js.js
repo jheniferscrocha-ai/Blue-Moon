c08842dddb933ab9cc5e593024b915c35617f74b
jheniferscrocha-ai <jhenifersc.rocha@gmail.com>
2026-09-18 08:57:23 -0300

    Refactor service worker event listeners

--- sw.js.js
+++ sw.js.js
@@ -1,26 +1,13 @@
-const CACHE_NAME = 'blue-moon-cache-v1';
-const ASSETS_TO_CACHE = [
-  '/Blue-Moon/',
-  '/Blue-Moon/index.html'
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (e) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE).catch(() => {});
-    })
-  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request).catch(() => caches.match('/Blue-Moon/'));
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
